145e9a8d7a6426f3dcc3684f388818c36dea173d
BhavyaLikhitha <bhavyalikhitha.bbl@gmail.com>
2025-01-22 18:02:14 -0500

    signup done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,21 +1,32 @@
-import './App.css'
-import Hero from './components/hero/hero'
-
-import JobTracker from './components/job'
-import Navbar from './components/navbar/nav'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Hero from "./components/hero/hero";
+import JobTracker from "./components/job";
+import Navbar from "./components/navbar/nav";
+import Signup from "./components/signup/signup";
 
 function App() {
-
-
   return (
-    <>
-    <Navbar/>
-    <Hero/>
-     <JobTracker/>
-    
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Base route displays Hero and Job Tracker */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <JobTracker />
+            </>
+          }
+        />
 
-    </>
-  )
+        {/* Signup route displays the Signup page */}
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- app/src/components/navbar/nav.jsx
+++ app/src/components/navbar/nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./nav.css";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -9,9 +9,17 @@ const Navbar = () => {
           ApplyTrack<span className="green-dot">●</span>
         </span>
       </div>
-      <div className="navbar-buttons">
+      {/* <div className="navbar-buttons">
         <button className="login-btn">Login</button>
         <button className="signup-btn">Sign Up</button>
+      </div> */}
+       <div className="navbar-buttons">
+        <Link to="/login" className="login-btn">
+          Login
+        </Link>
+        <Link to="/signup" className="signup-btn">
+          Sign Up
+        </Link>
       </div>
     </nav>
   );
