aa44d9417052e65382de355b9f8472fa959fafc7
BhavyaLikhitha <bhavyalikhitha.bbl@gmail.com>
2025-01-22 18:13:14 -0500

    added navigation to home page

--- app/src/components/navbar/nav.jsx
+++ app/src/components/navbar/nav.jsx
@@ -4,15 +4,13 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
-        <span className="brand-name">
-          ApplyTrack<span className="green-dot">●</span>
-        </span>
+       <div className="navbar-brand">
+        <Link to="/" className="brand-link">
+          <span className="brand-name">
+            ApplyTrack<span className="green-dot">●</span>
+          </span>
+        </Link>
       </div>
-      {/* <div className="navbar-buttons">
-        <button className="login-btn">Login</button>
-        <button className="signup-btn">Sign Up</button>
-      </div> */}
        <div className="navbar-buttons">
         <Link to="/login" className="login-btn">
           Login
