36e0cd43aa098ef269ef22a84b9922ae9c16cc63
BhavyaLikhitha <bhavyalikhitha.bbl@gmail.com>
2025-01-26 01:16:57 -0500

    defined dirname

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -20,7 +20,12 @@
 // export default upload;
 
 import multer from "multer";
+import { fileURLToPath } from "url";
 import path from "path";
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
