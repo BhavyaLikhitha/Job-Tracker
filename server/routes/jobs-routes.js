44dcceff38753d3af48c049d8e5901240d5a0fd7
BhavyaLikhitha <bhavyalikhitha.bbl@gmail.com>
2026-01-07 22:06:49 -0500

    added new route to search by company name

--- server/routes/jobs-routes.js
+++ server/routes/jobs-routes.js
@@ -10,13 +10,14 @@
 // export default router;
 
 import express from "express";
-import { addJob, getJobs, updateJobStatus } from "../controllers/job-controller.js";
+import { addJob, getJobs, updateJobStatus,searchJobs } from "../controllers/job-controller.js";
 import { authenticate } from "../services/auth-middleware.js";
 
 const router = express.Router();
 
 router.post("/add-job", authenticate, addJob);
 router.get("/get-job", authenticate, getJobs);
+router.get("/search-job", authenticate, searchJobs);
 router.put("/update-job-status", authenticate, updateJobStatus);
 
 export default router;
