ce2214df1d0d178dabcdf1f21979ddf7f0b690f6
BhavyaLikhitha <bhavyalikhitha.bbl@gmail.com>
2026-01-08 20:25:15 -0500

    modified API Endpoints according to vercel

--- server/index.js
+++ server/index.js
@@ -93,7 +93,6 @@ const port = process.env.PORT || 3000;
 const corsOptions = {
   origin: "https://job-tracker-coop-search.vercel.app", // Your frontend origin
   // origin : "http://localhost:5173", // For local testing
-  // origin: ["http://localhost:5173", "https://job-tracker-coop-search.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow credentials
